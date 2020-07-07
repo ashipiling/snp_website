63974f685033e8842743e32bdfc4b478402d6622
ashipiling <3130100174@zju.edu.cn>
2020-07-07 16:51:29 +0800

    upload

--- web/views.py
+++ web/views.py
@@ -32,17 +32,28 @@ def detail_lm(request):
     snp_list = SNP.objects.all()
     return render( request, 'web/index.html', context={'welcome': snp_list} )
 
-def upload_lh(request):
+def upload(request):
     #待修改
     print("前端数据: ", request.POST)
     print("file:", request.FILES)
  
     for item in request.FILES:
+
         obj = request.FILES.get(item)      # 获取要写入的文件
-        filename = obj.name            # 获取文件名
-        f = open(filename, 'rb')
-        for line in obj.chunks():      # 分块写入
-            f.write(line)
-        f.close()
- 
-    return render(request, "index.html")
+        #filename = obj.name            # 获取文件名
+        #f = open(filename, 'rb')
+
+        for line in obj.chunks():
+
+            #f.write(line)
+            info = line.split()
+            s = SNP(location = info[1], snp_type = info[0],
+                    snp_site_1 = info[2],snp_site_2 = info[3],snp_site_3 = info[4],snp_site_4 = info[5],
+                    snp_site_5 = info[6],snp_site_6 = info[7],snp_site_7 = info[8],snp_site_8 = info[9],
+                    snp_site_9 = info[10],snp_site_10 = info[11],snp_site_11 = info[12],snp_site_12 = info[13],
+                    snp_site_13 = info[14],snp_site_14 = info[15],snp_site_15 = info[16],snp_site_16 = info[17],)
+            s.author = 1
+            s.save()
+        #f.close()
+
+    return HttpResponse('OK')
