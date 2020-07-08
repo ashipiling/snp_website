493e98e56be9a4530a10c38f0cb6d39e089e094f
ashipiling <3130100174@zju.edu.cn>
2020-07-08 13:10:14 +0800

    views.py

--- web/views.py
+++ web/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from .models import SNP
 from django.contrib.auth.models import User
 from django.utils import timezone
+import xlrd
 
 
 def index(request):
@@ -36,19 +37,26 @@ def detail_lm(request):
 
 def upload(request):
 
-    for item in request.FILES:
 
-        obj = request.FILES.get(item)
+    filename = request.POST.get('fileName')
+    # 获取file类型的input标签值，即文件内容
+    file = request.FILES.get('fileContent')
 
-        for line in obj.chunks():
-            info = line.split()
-            s = SNP(location = info[1], snp_type = info[0], isolated_time = timezone.now(),created_time =timezone.now() , modified_time =timezone.now(),
-                    snp_site_1 = info[2],snp_site_2 = info[3],snp_site_3 = info[4],snp_site_4 = info[5],
-                    snp_site_5 = info[6],snp_site_6 = info[7],snp_site_7 = info[8],snp_site_8 = info[9],
-                    snp_site_9 = info[10],snp_site_10 = info[11],snp_site_11 = info[12],snp_site_12 = info[13],
-                    snp_site_13 = info[14],snp_site_14 = info[15],snp_site_15 = info[16],snp_site_16 = info[17],)
-            s.author = User.objects.get(username='luohao')
-            s.save()
+    workbook = xlrd.open_workbook( filename=None, file_contents=file.read() )
+    sheet1 = workbook.sheet_by_index(0)  # sheet索引从0开始
+    print(sheet1.name, sheet1.nrows, sheet1.ncols)
+
+    for i in range(1, sheet1.nrows):
+        info = sheet1.row_values(i)
+        s = SNP(location=info[2], snp_type=int(info[1]), isolated_time=timezone.now(), created_time=timezone.now(),
+                modified_time=timezone.now(),
+                snp_site_1=info[4], snp_site_2=info[5], snp_site_3=info[6], snp_site_4=info[7],
+                snp_site_5=info[8], snp_site_6=info[9], snp_site_7=info[10], snp_site_8=info[11],
+                snp_site_9=info[12], snp_site_10=info[12], snp_site_11=info[14], snp_site_12=info[15],
+                snp_site_13=info[16], snp_site_14=info[17], snp_site_15=info[18], snp_site_16=info[19], )
+        s.author = User.objects.get(username='luohao')
+        s.save()
+
 
     return HttpResponse('OK')
 
